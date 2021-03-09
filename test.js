9f02aafbcd8e9fb70892b43a1d3eff8cb639b4df
KevinFan <kevinypfan@gmail.com>
2021-03-09 16:16:41 +0800

    delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const LineNotifyService = require("./services/LineNotifyService");
-
-const lineNotifyService = new LineNotifyService(
-  process.env.LINE_NOTIFY_TOKENS.split(",")
-);
-
-lineNotifyService.sendLineNotify("test message");
